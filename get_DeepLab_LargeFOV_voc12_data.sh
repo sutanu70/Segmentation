2a7ce5af0ce6f1d137f54160e1066afb67d5b016
Martin Keršner <m.kersner@gmail.com>
2016-03-11 16:44:43 +0900

    changed init model; download only missing files

--- get_DeepLab_LargeFOV_voc12_data.sh
+++ get_DeepLab_LargeFOV_voc12_data.sh
@@ -4,23 +4,27 @@
 
 # DeepLab-LargeFOV
 NET_ID=DeepLab-LargeFOV 
+INIT_PATH=init
 MODEL_PATH=exper/voc12/model/${NET_ID}
 CONFIG_PATH=exper/voc12/config/${NET_ID}
 
-mkdir ${NET_ID}
+mkdir -p ${NET_ID}
+mkdir -p ${INIT_PATH}
 mkdir -p ${MODEL_PATH}
 mkdir -p ${CONFIG_PATH}
 
 cd ${NET_ID}
 
-wget http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/solver.prototxt
-wget http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/solver2.prototxt
-wget http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/test.prototxt
-wget http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/train.prototxt
-wget http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/train2_iter_8000.caffemodel
+wget -nc http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/solver.prototxt
+wget -nc http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/solver2.prototxt
+wget -nc http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/test.prototxt
+wget -nc http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/train.prototxt
+
+cd ../${INIT_PATH}
+wget -nc http://ccvl.stat.ucla.edu/ccvl/init_models/vgg16_20M.caffemodel
 
 cd ../${MODEL_PATH}
-ln -s ../../../../${NET_ID}/train2_iter_8000.caffemodel init.caffemodel
+ln -s ../../../../${INIT_PATH}/vgg16_20M.caffemodel init.caffemodel
 
 cd ../../config/${NET_ID}
 ln -s ../../../../${NET_ID}/solver.prototxt solver.prototxt
