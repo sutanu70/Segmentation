43eb0cdb865e0074d668b3e0d2f1ce72e9d00168
Martin Keršner <m.kersner@gmail.com>
2016-03-11 11:51:36 +0900

    changed a structure and connections for the LargeFOV model, its net description and solver

--- get_DeepLab_LargeFOV_voc12_data.sh
+++ get_DeepLab_LargeFOV_voc12_data.sh
@@ -7,10 +7,11 @@ NET_ID=DeepLab-LargeFOV
 MODEL_PATH=exper/voc12/model/${NET_ID}
 CONFIG_PATH=exper/voc12/config/${NET_ID}
 
+mkdir ${NET_ID}
 mkdir -p ${MODEL_PATH}
 mkdir -p ${CONFIG_PATH}
 
-cd ${MODEL_PATH}
+cd ${NET_ID}
 
 wget http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/solver.prototxt
 wget http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/solver2.prototxt
@@ -18,10 +19,11 @@ wget http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/test.prototxt
 wget http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/train.prototxt
 wget http://ccvl.stat.ucla.edu/ccvl/DeepLab-LargeFOV/train2_iter_8000.caffemodel
 
-ln -s train2_iter_8000.caffemodel init.caffemodel
+cd ../${MODEL_PATH}
+ln -s ../../../../${NET_ID}/train2_iter_8000.caffemodel init.caffemodel
 
 cd ../../config/${NET_ID}
-ln -s ../../model/${NET_ID}/solver.prototxt solver.prototxt
-ln -s ../../model/${NET_ID}/train.prototxt train.prototxt
+ln -s ../../../../${NET_ID}/solver.prototxt solver.prototxt
+ln -s ../../../../${NET_ID}/train.prototxt train.prototxt
 
 cd ../../../../
