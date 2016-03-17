8e2db9c69ca4ebd336fc71e0d68d4352d78229c6
Martin Keršner <m.kersner@gmail.com>
2016-03-17 17:43:26 +0900

    prepare lists for both datasets

--- prepare_voc12_data_lists.sh
+++ prepare_voc12_data_lists.sh
@@ -5,10 +5,16 @@
 LIST_DIR=exper/voc12/list
 
 rm ${LIST_DIR}/*.txt
-cp ${LIST_DIR}/original/*.txt ${LIST_DIR}/
+cp ${LIST_DIR}/original/*.txt ${LIST_DIR}
 
-for FILE in ${LIST_DIR}/*.txt; do
- sed -i 's/jpg/png/g' $FILE
- sed -i 's/JPEGImages/images/g' $FILE
- sed -i 's/SegmentationClassAug/labels/g' $FILE
+for pfile in "val.txt" "trainval.txt" "train.txt" "test.txt"; do
+  FILE_PATH=$LIST_DIR/$pfile
+  sed -i 's/JPEGImages/images_orig/g' $FILE_PATH
+  sed -i 's/SegmentationClassAug/labels_orig/g' $FILE_PATH
+done
+
+for pfile in "trainval_aug.txt" "train_aug.txt"; do
+  FILE_PATH=$LIST_DIR/$pfile
+  sed -i 's/JPEGImages/images_aug/g' $FILE_PATH
+  sed -i 's/SegmentationClassAug/labels_aug/g' $FILE_PATH
 done
