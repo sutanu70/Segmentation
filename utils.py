3946ec69a6108c306894ee93a75ee861b3e5ed40
Martin Keršner <m.kersner@gmail.com>
2016-03-18 14:34:33 +0900

    convert ground truth labels from RGB notation to one-dimensional

--- utils.py
+++ utils.py
@@ -70,3 +70,15 @@ def load_binary_segmentation(bin_file, dtype='int16'):
       out[i] = np.uint8(struct.unpack('h', bf.read(2))[0])
 
     return out.reshape((rows, cols))
+
+def convert_from_color_segmentation(arr_3d):
+  arr_2d = np.zeros((arr_3d.shape[0], arr_3d.shape[1]), dtype=np.uint8)
+  palette = pascal_palette()
+
+  # slow!
+  for i in range(0, arr_3d.shape[0]):
+    for j in range(0, arr_3d.shape[1]):
+      key = (arr_3d[i,j,0], arr_3d[i,j,1], arr_3d[i,j,2])
+      arr_2d[i, j] = palette.get(key, 0) # default value if key was not found is 0
+
+  return arr_2d
