022d2e7a39ceb4f19fb927cf7d44780b290d1870
Martin Keršner <m.kersner@gmail.com>
2016-03-25 11:52:19 +0900

    apply for both datasets, PASCAL VOC 2012 and PASCAL VOC augmented

--- filter_lists.sh
+++ filter_lists.sh
@@ -9,14 +9,25 @@ LIST_SUB_DIR=$BASE/list_subset/
 
 mkdir -p $LIST_SUB_DIR
 
-SUB_SEGM_LIST=subset_data.txt
+SUB_AUG_DATA=sub_aug_data.txt
+SUB_ORIG_DATA=sub_orig_data.txt
 
-for file in test_id  test  train_aug  train  trainval_aug  trainval  val_id  val; do
+for file in train_aug trainval_aug; do
   LIST_ALL_FILE="$LIST_ALL_DIR""$file""$TXT_EXT"
   LIST_SUB_FILE="$LIST_SUB_DIR""$file""$TXT_EXT"
 
-  grep -f $SUB_SEGM_LIST $LIST_ALL_FILE > $LIST_SUB_FILE
+  grep -f $SUB_AUG_DATA $LIST_ALL_FILE > $LIST_SUB_FILE
 
   # for training using limited number of classes only augmented dataset is employed
   sed -i 's/labels_aug/labels_sub_aug/g' $LIST_SUB_FILE
+done
+
+for file in test_id test train trainval val_id val; do
+  LIST_ALL_FILE="$LIST_ALL_DIR""$file""$TXT_EXT"
+  LIST_SUB_FILE="$LIST_SUB_DIR""$file""$TXT_EXT"
+
+  grep -f $SUB_ORIG_DATA $LIST_ALL_FILE > $LIST_SUB_FILE
+
+  # for training using limited number of classes only augmented dataset is employed
+  sed -i 's/labels_orig/labels_sub_orig/g' $LIST_SUB_FILE
 done
