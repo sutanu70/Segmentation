973d7d8360402d306f7e3f4762181690bb45f2a8
Martin Keršner <m.kersner@gmail.com>
2016-04-05 11:31:23 +0900

    refactoring only for strong annotations

--- run_pascal_strong.sh
+++ run_pascal_strong.sh
@@ -1,37 +1,35 @@
 #!/usr/bin/env sh
 
+# Modified version from https://bitbucket.org/deeplab/deeplab-public
+# Martin Kersner, m.kersner@gmail.com
+# 2016/04/05
+
 CAFFE_BIN=code/.build_release/tools/caffe.bin
 EXP=exper/voc12
-NUM_LABELS=4 # 21
 DATA_ROOT=exper/voc12/data
+GPU_ID=0
+
+# Specify number of classes
+NUM_LABELS=21         # all classes
+#NUM_LABELS=4         # 3 classes + 1 background
+
+#LIST_SUFFIX=         # all classes
+LIST_SUFFIX=_subset   # only for limited number of classes
+
+# Specify which dataset use for training
+#TRAIN_SET_SUFFIX=    # original PASCAL VOC 2012 dataset
+TRAIN_SET_SUFFIX=_aug # augmented PASCAL VOC dataset
 
 # Specify which model to train
-NET_ID=DeepLab-LargeFOV # Martin Kersner, 2016/03/10
-
-#LIST_SUFFIX=
-LIST_SUFFIX=_subset
-#TRAIN_SET_SUFFIX=
-TRAIN_SET_SUFFIX=_aug
-
-TRAIN_SET_STRONG=train
-#TRAIN_SET_STRONG=train200
-#TRAIN_SET_STRONG=train500
-#TRAIN_SET_STRONG=train750
-#TRAIN_SET_STRONG=train1000
-
-#TRAIN_SET_WEAK_LEN=5000
-
-# GPU ID
-DEV_ID=0
+NET_ID=DeepLab-LargeFOV
 
 # Run
 RUN_TRAIN=1
 RUN_TEST=0
-RUN_TRAIN2=0
+RUN_TRAIN2=1
 RUN_TEST2=0
-RUN_SAVE=0
 
-# Create directories
+# Create directories ###########################################################
 CONFIG_DIR=${EXP}/config/${NET_ID}
 MODEL_DIR=${EXP}/model/${NET_ID}
 LOG_DIR=${EXP}/log/${NET_ID}
@@ -42,21 +40,11 @@ done
 
 export GLOG_log_dir=${LOG_DIR}
 
-################################################################################
-
-# Training #1 (on train_aug)
+# Training #1 (on train_aug) ###################################################
 if [ ${RUN_TRAIN} -eq 1 ]; then
   LIST_DIR=${EXP}/list${LIST_SUFFIX}
   TRAIN_SET=train${TRAIN_SET_SUFFIX}
 
-  #if [ -z ${TRAIN_SET_WEAK_LEN} ]; then
-	#  TRAIN_SET_WEAK=${TRAIN_SET}_diff_${TRAIN_SET_STRONG}
-	#  comm -3 ${LIST_DIR}/${TRAIN_SET}.txt ${LIST_DIR}/${TRAIN_SET_STRONG}.txt > ${LIST_DIR}/${TRAIN_SET_WEAK}.txt
-  #else
-	#  TRAIN_SET_WEAK=${TRAIN_SET}_diff_${TRAIN_SET_STRONG}_head${TRAIN_SET_WEAK_LEN}
-	#  comm -3 ${LIST_DIR}/${TRAIN_SET}.txt ${LIST_DIR}/${TRAIN_SET_STRONG}.txt | head -n ${TRAIN_SET_WEAK_LEN} > ${LIST_DIR}/${TRAIN_SET_WEAK}.txt
-  #fi
-  
   MODEL=${EXP}/model/${NET_ID}/init.caffemodel
 
   echo "Training net ${EXP}/${NET_ID}"
@@ -67,7 +55,7 @@ if [ ${RUN_TRAIN} -eq 1 ]; then
 
   CMD="${CAFFE_BIN} train \
     --solver=${CONFIG_DIR}/solver_${TRAIN_SET}.prototxt \
-    --gpu=${DEV_ID}"
+    --gpu=${GPU_ID}"
 
 	if [ -f ${MODEL} ]; then
 	    CMD="${CMD} --weights=${MODEL}"
@@ -76,7 +64,7 @@ if [ ${RUN_TRAIN} -eq 1 ]; then
 	echo Running ${CMD} && ${CMD}
 fi
 
-# Test #1 specification (on val or test)
+# Test #1 specification (on val or test) #######################################
 if [ ${RUN_TEST} -eq 1 ]; then
   for TEST_SET in val; do
 	  TEST_ITER=`cat exper/voc12/list${LIST_SUFFIX}/${TEST_SET}.txt | wc -l`
@@ -97,25 +85,17 @@ if [ ${RUN_TEST} -eq 1 ]; then
 	  CMD="${CAFFE_BIN} test \
       --model=${CONFIG_DIR}/test_${TEST_SET}.prototxt \
       --weights=${MODEL} \
-      --gpu=${DEV_ID} \
+      --gpu=${GPU_ID} \
       --iterations=${TEST_ITER}"
 
 	  echo Running ${CMD} && ${CMD}
   done
 fi
 
-# Training #2 (finetune on trainval_aug)
+# Training #2 (finetune on trainval_aug) #######################################
 if [ ${RUN_TRAIN2} -eq 1 ]; then
   LIST_DIR=${EXP}/list${LIST_SUFFIX}
   TRAIN_SET=trainval${TRAIN_SET_SUFFIX}
-
-  #if [ -z ${TRAIN_SET_WEAK_LEN} ]; then
-	#  TRAIN_SET_WEAK=${TRAIN_SET}_diff_${TRAIN_SET_STRONG}
-	#  comm -3 ${LIST_DIR}/${TRAIN_SET}.txt ${LIST_DIR}/${TRAIN_SET_STRONG}.txt > ${LIST_DIR}/${TRAIN_SET_WEAK}.txt
-  #else
-	#  TRAIN_SET_WEAK=${TRAIN_SET}_diff_${TRAIN_SET_STRONG}_head${TRAIN_SET_WEAK_LEN}
-	#  comm -3 ${LIST_DIR}/${TRAIN_SET}.txt ${LIST_DIR}/${TRAIN_SET_STRONG}.txt | head -n ${TRAIN_SET_WEAK_LEN} > ${LIST_DIR}/${TRAIN_SET_WEAK}.txt
-  #fi
 
   MODEL=${EXP}/model/${NET_ID}/init2.caffemodel
 
@@ -132,14 +112,13 @@ if [ ${RUN_TRAIN2} -eq 1 ]; then
   CMD="${CAFFE_BIN} train \
     --solver=${CONFIG_DIR}/solver2_${TRAIN_SET}.prototxt \
     --weights=${MODEL} \
-    --gpu=${DEV_ID}"
+    --gpu=${GPU_ID}"
 
 	echo Running ${CMD} && ${CMD}
 fi
 
-# Test #2 on official test set
+# Test #2 on official test set #################################################
 if [ ${RUN_TEST2} -eq 1 ]; then
-  #for TEST_SET in val test; do # Martin Kersner, 2016/03/23
   for TEST_SET in val; do
 	  TEST_ITER=`cat exper/voc12/list${LIST_SUFFIX}/${TEST_SET}.txt | wc -l`
 	  MODEL=${EXP}/model/${NET_ID}/test2.caffemodel
@@ -158,28 +137,9 @@ if [ ${RUN_TEST2} -eq 1 ]; then
 	  CMD="${CAFFE_BIN} test \
       --model=${CONFIG_DIR}/test_${TEST_SET}.prototxt \
       --weights=${MODEL} \
-      --gpu=${DEV_ID} \
+      --gpu=${GPU_ID} \
       --iterations=${TEST_ITER}"
 
 	  echo Running ${CMD} && ${CMD}
   done
-fi
-
-# Translate and save the model
-if [ ${RUN_SAVE} -eq 1 ]; then
-  MODEL=${EXP}/model/${NET_ID}/test2.caffemodel
-
-  if [ ! -f ${MODEL} ]; then
-	  MODEL=`ls -t ${EXP}/model/${NET_ID}/train*_iter_*.caffemodel | head -n 1`
-  fi
-
-  MODEL_DEPLOY=${EXP}/model/${NET_ID}/deploy.caffemodel
-  echo "Translating net ${EXP}/${NET_ID}"
-
-  CMD="${CAFFE_BIN} save \
-    --model=${CONFIG_DIR}/deploy.prototxt \
-    --weights=${MODEL} \
-    --out_weights=${MODEL_DEPLOY}"
-
-	echo Running ${CMD} && ${CMD}
 fi
